cdfd1f5b416c62a22ffe2452b980f8600c8155fd
JhonathanHerrera <herrerajhonathan@ufl.edu>
2025-01-30 09:39:06 -0500

    Finished Chpater 13

--- Day9.cpp
+++ Day9.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <string>
+#include <string_view>
 
+struct Employee
+{
+	int id{};
+	int age{};
+	double wage{};
+};
+
+// Teach operator<< how to print a Color
+// std::ostream is the type of std::cout, std::cerr, etc...
+// The return type and parameter type are references (to prevent copies from being made)
+std::ostream &operator<<(std::ostream &out, Color_ color)
+{
+	out << getColorName(color); // print our color's name to whatever output stream out
+	return out;					// operator<< conventionally returns its left operand
+
+	// The above can be condensed to the following single line:
+	// return out << getColorName(color)
+}
+
+std::ostream &operator<<(std::ostream &out, const Employee &e)
+{
+	out << e.id << ' ' << e.age << ' ' << e.wage;
+	return out;
+}
 struct Fraction
 {
 	int numerator{};
@@ -22,6 +48,7 @@ enum Color_
 	red_,
 	green_,
 	blue_, // trailing comma optional but recommended
+	black_,
 };
 
 enum Animal
@@ -34,6 +61,46 @@ enum Animal
 	giraffe = 5, // 5
 	chicken,	 // 6
 };
+
+constexpr std::string_view getColorName(Color_ color)
+{
+	switch (color)
+	{
+	case black_:
+		return "black";
+	case red_:
+		return "red";
+	case blue_:
+		return "blue";
+	default:
+		return "???";
+	}
+}
+
+enum Pet
+{
+	cat,   // 0
+	dog,   // 1
+	pig,   // 2
+	whale, // 3
+};
+
+constexpr std::string_view getPetName(Pet pet)
+{
+	switch (pet)
+	{
+	case cat:
+		return "cat";
+	case dog:
+		return "dog";
+	case pig:
+		return "pig";
+	case whale:
+		return "whale";
+	default:
+		return "???";
+	}
+}
 int main()
 {
 
@@ -71,10 +138,56 @@ int main()
 	Color_ cup{blue_};
 
 	/*
-	unscoped enumeators integral conversions
+	Converting an enumeartion to and from a string
 
+	Color shirt{ blue };
+
+	std::cout << "Your shirt is " << shirt << '\n';
+
+	This prints:
+
+	Your shirt is 2
+
+	How to fix this
+	*/
+
+	constexpr Color_ shirt{blue_};
+	std::cout << "Your shirt is " << getColorName(shirt) << '\n';
+
+	std::cout << "Enter a pet (0=cat, 1=dog, 2=pig, 3=whale): ";
+
+	int input{3};
+	// std::cin >> input; // input an integer
+
+	if (input < 0 || input > 3)
+		std::cout << "You entered an invalid pet\n";
+	else
+	{
+		Pet pet{static_cast<Pet>(input)}; // static_cast our integer to a Pet
+		std::cout << "You entered: " << getPetName(pet) << '\n';
+	}
+
+	/*
+	Introduciton to overloading the I/O operators
 
 	*/
+
+	Color_ shirt{blue_};
+	std::cout << "Your shirt is " << shirt << '\n'; // it works!
+
+	/*
+	Introduction to structs, members, and member selection
+	*/
+	Employee joe{};
+	joe.age = 32;
+
+	std::cout << joe.age << '\n';
+
+	/*
+	Struct aggregate initialization
+	*/
+	Employee joe{2, 28, 45000.0}; // list initialization using braced list (preferred)
+	std::cout << joe << '\n';
 
 	return 0;
 }
