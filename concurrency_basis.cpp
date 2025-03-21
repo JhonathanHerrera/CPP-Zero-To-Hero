9dc6061a8b06a1cfee7aab0f334e5393a0f22061
JhonathanHerrera <herrerajhonathan@ufl.edu>
2025-03-20 22:11:36 -0400

    More Thread details

--- concurrency_basis.cpp
+++ concurrency_basis.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
 /*
 
@@ -31,11 +32,69 @@ When multiple threads share resources (e.g., variables), they can interfere with
 std::mutex mtx; → Creates a mutex.
 
 std::lock_guard<std::mutex> lock(mtx); → Locks the mtx when modifying counter, preventing simultaneous updates.
+
+Mutexes & Locks in C++
+
+6. Why do we need mutexes?
+
+- When multiple threads access shared data at the same time, they can cause race conditions, leading to incorrect results.
+For example, if two threads are modifying a shared variable at the same time, the final value may be unpredictable.
+
+7. Mutex (std::mutex)
+- A mutex (mutual exclusion) ensures that only one thread accesses a shared resource at a time.
+
+8. std::lock_guard (Automatci Mutex Handling)
+
+Using mtx.lock() and mtx.unlock() manually is error-prone. If an exception occursm the mutex may not be unlocked
+- std::lock_guard<std::mutex> automatically locks and unlocks the mutex.
+
+9. std::unique_lock (More Flexible)
+
+If you need more control, use std::unique_lock<std::mutex>, which allows
+
+1. Deferred locking (lock later)
+2. Locking & unlocking manually
+3. Moving ownership of locks
+
+10. Deadlocks and How to avoid them
+- Deadlocks occurs when two or more threads wait on each other, and execution halts forever
+
+11. Avoiding Deadlocks
+
+Solution 1) Always Lock Mutexes in the Same Order
+- If all threads always lock mutexes in the same order, deadlocks wont occur
+
+Solution 2) Use std:try_lock
+- std::try_lock doesnt block- if it cants lock a mutex, it moves on
+
+12. std:condition_variable
+
+Why do we need it?
+ - a conditional variable allows one or more threads to wait (pause execution) until a condition is met
+
+Without condition variables:
+    - A thread that needs some data must continuously check (polling), which wastes CPU cycle
+
+With Condition variables:
+    - The thread sleeps until notified, making it efficient,
+
+13. basic usage of condition_variable
+- wakes up a waiting thread when the condition is met
 */
 
 int counter = 0;      // shared resource
 std::mutex mtx;       // Mutex to protect counter
 std::mutex print_mtx; // Mutex to protect counter
+std::mutex unique_lock;
+std::condition_variable cv;
+
+void uniqueLockFunction()
+{
+    std::unique_lock<std::mutex> lock(unique_lock);
+
+    // Do something
+    lock.unlock();
+}
 
 void printMessage()
 {
